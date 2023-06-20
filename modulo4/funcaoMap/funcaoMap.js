d8aa53156fa67defeaffa81f13d95c693c42052c
Marcos Pontes Junior <127991089+marcospontesjunior@users.noreply.github.com>
2023-06-20 19:52:14 +0100

    Update modulo4

---
+++ modulo4/funcaoMap/funcaoMap.js
@@ -0,0 +1,50 @@
+const nums = [1, 2, 3, 4, 5]; // array
+const dobro = (n) => n * 2;
+
+console.log(nums.map(dobro)); // dentro do map tem a função callback
+
+const nums1 = [1, 2, 3, 4, 5];
+const dobro1 = (n, i) => n * 2 + i; // i -> index [0, 1, 2, 3, 4]
+
+console.log(nums1.map(dobro1));
+
+const nomes = ["Ana", "Bia", "Gui", "Lucas", "Gabriel"];
+const primeiraLetra = (texto) => texto[0];
+const letra = nomes.map(primeiraLetra);
+console.log(nomes, letra);
+
+const compra = [
+  { nome: "Caneta", qtde: 10, preco: 7.99 },
+  { nome: "Impressora", qtde: 0, preco: 649.5 },
+  { nome: "Caderno", qtde: 4, preco: 27.1 },
+  { nome: "Lapis", qtde: 3, preco: 5.82 },
+  { nome: "Tesoura", qtde: 1, preco: 19.2 },
+];
+
+// usando map, mapear nome dos produtos e pegar os valores pelo preço
+
+const nomeProduto = (nome) => nome.nome;
+
+console.log(compra.map(nomeProduto));
+
+const valorTotal = (objeto) => objeto.qtde * objeto.preco;
+
+console.log(compra.map(valorTotal));
+
+// criando uma função map
+
+Array.prototype.novoMap = function (fn) {
+  const novoArrary = [];
+  for (let i = 0; i < this.length; i++) {
+    novoArrary.push(fn(this[(i, i, this)]));
+  }
+  return novoArrary;
+};
+
+const nomeProduto1 = (nome) => nome.nome;
+
+console.log(compra.map(nomeProduto1));
+
+const valorTotal1 = (objeto) => objeto.qtde * objeto.preco;
+
+console.log(compra.map(valorTotal1));
