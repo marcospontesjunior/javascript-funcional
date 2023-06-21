29c1a327e037b70ab0b8779037c42be6d7e27a16
Marcos Pontes Junior <127991089+marcospontesjunior@users.noreply.github.com>
2023-06-21 23:27:15 +0100

    Update exercicioParte4

---
+++ exercicioParte4/resolucao4.js
@@ -0,0 +1,47 @@
+// Array
+
+const compra = [
+  { nome: "Caneta", qtde: 10, preco: 7.99, fragil: true },
+  { nome: "Impressora", qtde: 1, preco: 649.5, fragil: true },
+  { nome: "Caderno", qtde: 4, preco: 27.1, fragil: false },
+  { nome: "Lapis", qtde: 3, preco: 5.82, fragil: false },
+  { nome: "Tesoura", qtde: 1, preco: 19.2, fragil: true },
+];
+
+// declarando variáveis
+
+const fragil = (fragil) => fragil.fragil === true;
+
+// mapeando os itens frágil = true
+
+const produtosFrageis = compra.filter(fragil).map((nome) => nome.nome);
+
+console.log("Os produtos frágeis são:", produtosFrageis);
+
+// achando valor total dos produtos frágeis
+
+const valorTotalPerItem = compra
+  .filter(fragil)
+  .map((soma) => soma.qtde * soma.preco);
+
+console.log("com um valor total por quantidade item de", valorTotalPerItem);
+
+// valor média de cada item
+
+const media = compra
+  .filter(fragil)
+  .map((soma) => soma.qtde * soma.preco)
+  .reduce(
+    (acc, el) => {
+      const novaQtde = acc.qtde + 1;
+      const novoTotal = acc.total + el;
+      return {
+        qtde: novaQtde,
+        total: novoTotal,
+        media: novoTotal / novaQtde,
+      };
+    },
+    { qtde: 0, total: 0, media: 0 }
+  );
+
+console.log(media);
